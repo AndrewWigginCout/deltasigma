908b2b8fe0c3e48a02af255341f961b8e97acd99
AndrewWigginCout <91389007+AndrewWigginCout@users.noreply.github.com>
2022-11-24 19:46:08 -0800

    form writes to mongodb

--- servertest.js
+++ servertest.js
@@ -1,13 +1,65 @@
 const express = require('express')
 const app = express()
+const fs   = require('fs')
+require('dotenv').config()
+const Contact = require('./models/contact')
+
+var contact_html = fs.readFileSync("perfect-learn/contact.html")
+var index_html = fs.readFileSync("perfect-learn/index.html")
+
+function fixnames (x){
+  return{
+    name: x['your name'],
+    number: x['Phone number'],
+    email: x['Email']
+  }
+}
+
+app.get('/index.html', (req,res)=>{
+  var bool = Object.keys(req.query).length === 0
+  if (bool){
+    res.setHeader("Content-Type", "text/html")
+    res.writeHead(200)
+    res.end(index_html)
+  }
+  else{
+    console.log('query=',req.query)
+    const dbobj = new Contact(fixnames(req.query))
+    dbobj.save().then(x => {
+      res.status(200).send(`<h3>Thank you for your interest<br>
+        You entered:<br>
+        ${JSON.stringify(x)}<br>
+        We will contact you shortly.`)
+    })
+  }
+})
+app.get('/contact.html', (req,res)=>{
+  var bool = Object.keys(req.query).length === 0
+  if (bool){
+    res.setHeader("Content-Type", "text/html")
+    res.writeHead(200)
+    res.end(contact_html)
+  }
+  else{
+    console.log('query=',req.query)
+    const dbobj = new Contact(fixnames(req.query))
+    dbobj.save().then(x => {
+      console.log(x)
+      res.status(200).send(`<h3>Thank you for your interest<br>
+        You entered:<br>
+        ${JSON.stringify(x)}<br>
+        We will contact you shortly.`)
+    })
+  }
+})
 app.use(express.static('perfect-learn'))
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send(request)
+  response.status(404).send(`<h1>404</h1>`)
   }
   app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8081
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
